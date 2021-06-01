87f54a9e7536a17ae7634f973104f395b6cd7175
Chris <cochsenreither@protonmail.com>
2021-06-01 16:52:42 +0000

    initial commit

---
+++ homepage.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', (req, res) => {
+    res.render('index', { title: 'ochsec.info' })
+})
+
+app.listen(port, () => console.log(`Homepage running at http://localhost:${port}`))
